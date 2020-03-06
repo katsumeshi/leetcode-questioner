d72d862d429c3524e912aefc0facff0dce5ad335
Yuki Matsushita <katsumeshi@gmail.com>
2020-03-06 09:41:50 -0800

    update layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect, useRef } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import problems from "./problems.json";
-import { MenuItem, FormControl, Select, InputLabel, Button } from "@material-ui/core";
+import { MenuItem, FormControl, Select, InputLabel, Button, Container, Typography, Grid } from "@material-ui/core";
 
 const useStyles = makeStyles(theme => ({
 	formControl: {
@@ -16,7 +16,7 @@ const useStyles = makeStyles(theme => ({
 const Index = () => {
 	const classes = useStyles();
 	const [difficulty, setDifficulty] = useState(1);
-	const [quetion, setQuestion] = useState<any>(null);
+	const [question, setQuestion] = useState<any>(null);
 	const handleChange = event => {
 		setDifficulty(event.target.value);
 	};
@@ -26,31 +26,36 @@ const Index = () => {
 		setQuestion(filteredProblem[questionIndex]);
 	};
 	return (
-		<>
-			<p>Let's Play Leet Code</p>
-			<FormControl className={classes.formControl}>
-				<InputLabel id="demo-simple-select-label">Difficulty</InputLabel>
-				<Select labelId="demo-simple-select-label" id="demo-simple-select" value={difficulty} onChange={handleChange}>
-					<MenuItem value={1}>Easy</MenuItem>
-					<MenuItem value={2}>Medium</MenuItem>
-					<MenuItem value={3}>Hard</MenuItem>
-				</Select>
-			</FormControl>
-			<Button variant="outlined" onClick={handlePickOne}>
-				Pick one
-			</Button>
-			<div>
-				{quetion && (
-					<>
-						<a href={`https://leetcode.com/problems/${quetion.stat.question__title_slug}`}>{`${quetion.stat.question__title}`}</a>
-						<p>{`${quetion.difficulty.level}`}</p>
-					</>
-				)}
-			</div>
-			<button className="mdc-button mdc-button--outlined">
-				<span className="mdc-button__ripple"></span> Learn More
-			</button>
-		</>
+		<Container>
+			<Grid container spacing={3}>
+				<Grid container item xs={12} justify="center">
+					<p>Let's Play Leet Code</p>
+				</Grid>
+				<Grid container item xs={12} justify="center">
+					<FormControl className={classes.formControl}>
+						<InputLabel id="demo-simple-select-label">Difficulty</InputLabel>
+						<Select labelId="demo-simple-select-label" id="demo-simple-select" value={difficulty} onChange={handleChange}>
+							<MenuItem value={1}>Easy</MenuItem>
+							<MenuItem value={2}>Medium</MenuItem>
+							<MenuItem value={3}>Hard</MenuItem>
+						</Select>
+					</FormControl>
+					<Button variant="outlined" onClick={handlePickOne}>
+						Pick one
+					</Button>
+				</Grid>
+				<Grid container item xs={12} justify="center">
+					<div>
+						{question && (
+							<>
+								<a href={`https://leetcode.com/problems/${question.stat.question__title_slug}`}>{`${question.stat.question__title}`}</a>
+								<p>{`${question.difficulty.level}`}</p>
+							</>
+						)}
+					</div>
+				</Grid>
+			</Grid>
+		</Container>
 	);
 };
 
